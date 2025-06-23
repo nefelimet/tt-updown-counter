31e7a3efb9b05f49d1c2232c0ca889efe88818c6
Nefeli Metallidou <47782076+nefelimet@users.noreply.github.com>
2025-06-23 23:13:15 +0300

    Fix bit-slicing error

--- test/test.py
+++ test/test.py
@@ -15,8 +15,8 @@ async def test_project(dut):
     updn_port = dut.ui_in[5]
     ld_port = dut.ui_in[4]
     rst_port = dut.ui_in[3]
-    data_in_port = dut.ui_in[2:0]
-    data_out_port = dut.ui_out[2:0]
+    data_in_port = [dut.ui_in[2], dut.ui_in[1], dut.ui_in[0]]
+    data_out_port = [dut.ui_out[2], dut.ui_out[1], dut.ui_out[0]]
 
     # Set the clock period to 20 ns (50 MHz)
     clock = Clock(dut.clk, 20, units="ns")
@@ -35,16 +35,27 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Set the input values you want to test
-    data_in_port.value = 3
+    value = 3
+    data_in_port[0].value = (value >> 0) & 1
+    data_in_port[1].value = (value >> 1) & 1
+    data_in_port[2].value = (value >> 2) & 1
     
     ld_port.value = 0
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
-    assert data_out_port.value == 3
     
-    data_in_port.value = 6
+    exp_value = (data_out_port[2].value << 2) | (data_out_port[1].value << 1) | data_out_port[0].value
+    assert exp_value == 3
+    
+    value = 6
+    data_in_port[0].value = (value >> 0) & 1
+    data_in_port[1].value = (value >> 1) & 1
+    data_in_port[2].value = (value >> 2) & 1
+    
     await ClockCycles(dut.clk, 1)
-    assert data_out_port.value == 6
+    
+    exp_value = (data_out_port[2].value << 2) | (data_out_port[1].value << 1) | data_out_port[0].value
+    assert exp_value == 6
 
     
